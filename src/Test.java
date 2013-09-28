46d0ff39adb62305634ca7722030994f980a69d2
Josh Newlin <josh062395@gmail.com>
2013-09-27 21:52:36 -0400

    second commit

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,7 @@
 public class Test {
 	
 	public static void main(String[] a){
-		System.out.println("Please work");
+		System.out.println("I Changed the string and now it will mess things up (but hopefully not)");
 	}
 
 }
